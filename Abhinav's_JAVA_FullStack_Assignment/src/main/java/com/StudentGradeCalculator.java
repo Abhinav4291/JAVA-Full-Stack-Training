1ea58e42b6b7ba5bd7ed9edcb85d459be12d62b9
Abhinav <132575576+Abhinav4291@users.noreply.github.com>
2026-02-23 16:43:32 +0530

    Update StudentGradeCalculator.java

--- Abhinav's_JAVA_FullStack_Assignment/src/main/java/com/StudentGradeCalculator.java
+++ Abhinav's_JAVA_FullStack_Assignment/src/main/java/com/StudentGradeCalculator.java
@@ -6,16 +6,12 @@ import java.util.List;
 public class StudentGradeCalculator {
 
     private List<Integer> marks = new ArrayList<>();
-
-    // Add marks
     public void addMarks(int marks) {
         if (marks < 0) {
             throw new IllegalArgumentException("Marks cannot be negative");
         }
         this.marks.add(marks);
     }
-
-    // Calculate average
     public double calculateAverage() {
         if (marks.isEmpty()) {
             throw new IllegalStateException("No marks added");
@@ -28,8 +24,6 @@ public class StudentGradeCalculator {
 
         return (double) sum / marks.size();
     }
-
-    // Get result
     public String getResult() {
         double average = calculateAverage();
         if (average >= 40) {
